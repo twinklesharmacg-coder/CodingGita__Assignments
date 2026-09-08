11c6f89f6a7e3a94d0a2caa4608e014179cbc755
Twinkle <twinkle.sharma.cg@gmail.com>
2026-09-08 09:41:10 +0530

    Uploaded Assignment-1

---
+++ Semester 1/Python/Assignment-01/Assignment.py
@@ -0,0 +1,511 @@
+# # Topic 1 :  Variables and Variable Naming
+
+# # <-----------Question-1------------>
+
+# name = "Raju"
+# print("Name:", name)
+
+# # <-----------Question-2------------->
+
+# student_name = "Aman"
+# print("Student Name:", student_name)
+
+# # <----------Question-3---------------->
+
+# student_name = "Rahul"
+# print("Student Name:", student_name)
+
+# # <---------Question-4--------------->
+
+# student_name = "Ravi"
+# print("Student Name:", student_name)
+
+# # <---------Question-5--------------->
+
+# student_name = "Rohan"
+# print("Student Name:", student_name)
+
+# # <---------Question-6--------------->
+
+# student_name = "Neha"
+# student_age = 19
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+
+# # <---------Question-7--------------->
+
+# Student_Name = "Karan"
+# student_name = "Raj"
+
+# print("Student 1:", Student_Name)
+# print("Student 2:", student_name)
+
+# # <---------Question-8--------------->
+
+# student_name = "Priya"
+# student_age = 19
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+
+# # <---------Question-9--------------->
+
+# name = "Amit"
+# student_age = 18
+
+# print("Name:", name)
+# print("Age:", student_age)
+
+# # <---------Question-10--------------->
+
+# college_name = "ABC College"
+# student_name = "Meera"
+
+# print("College:", college_name)
+# print("Student:", student_name)
+
+# # <---------Question-11--------------->
+
+# first_name = "Raj"
+# last_name = "Patel"
+
+# print("Full Name:", first_name, last_name)
+
+# # <---------Question-12--------------->
+
+# first_name = "Raj"
+# last_name = "Patel"
+
+# print("Full Name:", first_name, last_name)
+
+
+
+
+# # ||||||||||||||||||||||||||||||  Topic - 2 : print()  |||||||||||||||||||||||||||||||||||
+
+
+# # <------------Question-13--------------->
+
+
+# name = "Rahul Raj"
+# age = 18
+# course = "B.TECH"
+# city = "Patna"
+
+# print("My name is", name + ".","I am",age,"year old. I am studying",course,"and I live in",city)
+
+
+# # <------------Question-15--------------->
+
+# name = "Priya Sharma"
+# age = 19
+# gender = "Female"
+# city = "Jaipur"
+
+# print("My name is",name+".","I am",age,"years old. I am",gender,"and I live in Jaipur")
+
+
+# # <------------Question-14--------------->
+
+# name = "Aman Kumar"
+# roll_num = 101
+# branch = "Computer Science"
+# semester = 1
+
+# print("My name is",name,". My roll number is", str(roll_num)+". I am studying",branch,"in semester",semester+str(semester))
+
+
+# # <------------Question-16--------------->
+
+# brand = "Samsung"
+# model = "Galaxy A55"
+# ram = "8 GB"
+# storage = "256 GB"
+
+# print("I have a",brand,model,"with",ram,"and",storage,"storage.")
+
+
+# # <------------Question-17--------------->
+
+# name = "Rohit Kumar"
+# role = "Software Developer"
+# company="ABC Technologies"
+# experience="2 years"
+
+# print("my name is",name+". I work as a",role,"at",company,"and I have",experience,"of experience.")
+
+
+# # <------------Question-18--------------->
+
+# college="XYZ Institute of Technology"
+# course="B.Tech"
+# branch="information Technology"
+# city="Ahmedabad"
+
+# print("I am studying",course,"in",brand,"at",college,"in",city)
+
+
+# # <------------Question-19--------------->
+
+# product="Laptop"
+# brand="HP"
+# price=55000
+# quantity=1
+
+# print("I bought",quantity,brand,product,"for",price,"rupees.")
+
+
+# # <------------Question-20--------------->
+
+# name="Rahul Raj"
+# father_name="Rajesh kumar"
+# mother_name="Sunita Kumar"
+# city="patna"
+
+# print("My name is",name,". My father's name is",father_name+".","My Mother name is",mother_name+". I live in",city)
+
+
+
+# # <------------Question-21--------------->
+
+# name="Neha Sharma"
+# Subject="Python Programming"
+# marks=88
+# grade="A"
+# percentage=88.0
+
+# print(name,"scored",marks,"marks in",Subject+". Her grade is",grade,"and her percentage is",str(percentage)+".")
+
+
+# #<--------------Question-22---------------->
+
+
+# brand="HP"
+# model="pavilion 15"
+# processor="Intel Core i5"
+# ram="16 GB"
+# storage="512 GB SSD"
+# price=65000
+
+# print("I have an",brand,model,"with",processor,",",ram,"RAM,",storage,", and it costs",price,"rupees.")
+
+
+# # <------------Question-23--------------->
+
+# movie="3 Idiots"
+# genre="Comedy Drama"
+# rating="8.4"
+# language="Hindi"
+# release_year="2009"
+
+# print(movie,"is a",genre,"in",language+". It was released in",release_year,"and has a rating of",rating+".")
+
+
+# # <------------Question-24--------------->
+
+# name="Arjun Patel"
+# age=19
+# college="ABC Institute of Technology"
+# branch="Computer Science"
+# semester=1
+# city="Ahmedabad"
+# percentage="87.5"
+# lang="Python"
+
+# print("My name is",name,". I am",age,"years old and I am studying",branch,"in semester",semester,"at",college,"in",city+". I scored",percentage,"percentage and my favourite programming language is",lang)
+
+
+
+# # # ||||||||||||||||||||||||||||||  Topic - 3 : Data Types and type()  |||||||||||||||||||||||||||||||||||
+
+
+
+# # <------------Question-21--------------->
+
+# name = "Riya"
+# age = 20
+# marks = 85.5
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+
+# # <------------Question-22--------------->
+
+
+# name = "Riya"
+# age = 20
+# marks = 85.5
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+
+# # <------------Question-23--------------->
+
+
+# age = "18"
+# marks = 85.5
+
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+
+# # <------------Question-24--------------->
+
+
+# age = 18
+# marks = "85.5"
+
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+
+# # <------------Question-25--------------->
+
+# name = "Ravi"
+# age = 18
+# marks = 90.5
+# passed = True
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Passed:", passed)
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(passed))
+
+# # <------------Question-26--------------->
+
+# name = "Ravi"
+# age = 18
+# marks = 90.5
+# passed = True
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Passed:", passed)
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(passed))
+
+# # <------------Question-27--------------->
+
+# student_name = "Karan"
+# student_age = 18
+# student_marks = 87.5
+
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+
+# print("Student Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+
+# # <------------Question-28--------------->
+
+# student_name = "Karan"
+# student_age = 18
+# student_marks = 87.5
+
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+
+# print("Student Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+
+# # <------------Question-29--------------->
+
+# name = "Riya"
+# age = 20
+
+# print("Name:", name)
+# print("Age:", age)
+
+# # <------------Question-30--------------->
+
+# name = "Aman"
+# age = 18
+
+# print("Name:", name)
+# print("Age:", age)
+
+# # <------------Question-31--------------->
+
+# student_name = "Rohan"
+# student_age = 19
+# student_city = "Ahmedabad"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("City:", student_city)
+
+# # <------------Question-32--------------->
+
+# student_name = "Neha"
+# student_age = 20
+# student_city = "Vadodara"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("City:", student_city)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("City Type:", type(student_city))
+
+# # <------------Question-33--------------->
+
+# student_name = "Amit"
+# student_age = 18
+# student_college = "XYZ College"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("College:", student_college)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+
+# # <------------Question-34--------------->
+
+# student_name = "Meera"
+# student_age = 20
+# student_college = "ABC College"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("College:", student_college)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("College Type:", type(student_college))
+
+# # <------------Question-35--------------->
+
+# student_name = "Raju"
+# student_age = 18
+# student_marks = 92.5
+# is_passed = True
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Passed:", is_passed)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+# print("Passed Type:", type(is_passed))
+
+# # <------------Question-36--------------->
+
+# student_name = "Raju"
+# student_age = 18
+# student_marks = 92.5
+# is_passed = True
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Passed:", is_passed)
+
+# # <------------Question-37--------------->
+
+# name = "Amit"
+# age = 18
+# marks = 91.5
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# # <------------Question-38--------------->
+
+# college_name = "ABC College"
+# student_name = "Riya"
+# student_age = 19
+
+# print("College:", college_name)
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("College Type:", type(college_name))
+
+# # <------------Question-39--------------->
+
+# class_name = "B.Tech"
+# student_name = "Ravi"
+
+# print("Class:", class_name)
+# print("Student:", student_name)
+
+# # <------------Question-40--------------->
+
+# student_name = "Ravi"
+# student_age = 18
+# student_marks = 89.5
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+
+# # <------------Question-41--------------->
+
+# student_name = "Ravi"
+# student_age = 18
+# student_marks = 89.5
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+
+# # <------------Question-42--------------->
+
+# name = "Karan"
+# age = 18
+# marks = 87.5
+# college_name = "ABC College"
+
+# print("Name:", name)
+# print("Age", age)
+# print("Marks:", marks)
+# print("College:", college_name)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+# print("College Type:", type(college_name))

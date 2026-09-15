64b4ed9f882ed311fe07690aa445bea7cd373b03
Twinkle <twinkle.sharma.cg@gmail.com>
2026-09-15 10:01:01 +0530

    Update and rename Topic-1 to Topic-1.py

---
+++ Semester 1/Python/Assignment-03/Topic-1.py
@@ -0,0 +1,51 @@
+# Q1. Predict the Output
+
+#Predict the output before running the code.
+
+
+a = 15
+b = 20
+
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+
+
+# Q2. Compare Expressions
+
+#Predict the output:
+
+
+x = 10
+y = 10
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x <= y)
+print(x >= y)
+
+
+# Q3. Comparison with Arithmetic
+
+#Predict the output:
+a = 10
+b = 5
+
+print(a + b == 15)
+print(a * b > 40)
+print(a - b != 5)
+print(a // b == 2)
+
+# Q4. String Comparison
+
+#Predict the output:
+
+print("Python" == "Python")
+print("Python" == "python")
+print("Hello" != "hello")
+
+

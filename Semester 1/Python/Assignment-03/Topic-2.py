153565e5219ef40c50cc1021dd95c3a023fb4f8a
Twinkle <twinkle.sharma.cg@gmail.com>
2026-09-15 19:09:10 +0530

    Create Topic-2.py

---
+++ Semester 1/Python/Assignment-03/Topic-2.py
@@ -0,0 +1,35 @@
+Q5. Trace the Value
+Predict the final value of x:
+
+x = 20      
+
+x += 10     ANS-X=30
+x -= 5      ANS-X=15
+x *= 2       ANS-X=40
+x //= 5      ANS-X=4
+
+print(x)
+
+# Q6. Assignment Operator Practice
+
+#Start with:
+
+```python
+marks = 50
+```
+
+Use assignment operators to:
+
+1. Increase marks by `10`
+2. Decrease marks by `5`
+3. Multiply marks by `2`
+
+Print the final value.
+
+---ANSWERS
+  marks+=10
+marks-=5
+marks*=2
+print(marks)
+
+

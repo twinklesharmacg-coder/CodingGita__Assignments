c417d728e208dcf5c6c714fece457ca6710ce935
Twinkle <twinkle.sharma.cg@gmail.com>
2026-09-19 00:06:07 +0530

    Create Topic-5.py

---
+++ Semester 1/Python/Assignment-03/Topic-5.py
@@ -0,0 +1,84 @@
+Q19. Basic Indexing
+Given:
+
+text = "PYTHON"
+Print:
+
+First character
+Second character
+Last character
+Second-last character
+
+--ANSWERS:
+print(text[0])
+print(text[1])
+print(text[-2])
+print(text[-1])
+
+
+Q20. Positive and Negative Indexing
+Given:
+
+text = "COMPUTER"
+Find the characters at:
+
+0
+3
+-1
+-3
+Write the Python expressions.
+--ANSWERS:
+print(text[0])
+print(text[3])
+print(text[-1])
+print(text[-3])
+
+
+Q21. Predict the Output
+text = "PYTHON"
+
+print(text[0])
+print(text[2])
+print(text[-1])
+print(text[-2])
+
+--ANSWERS:
+P
+T  
+N
+O
+Q22. Indexing a User Input
+Take a word from the user and print:
+
+First character
+Last character
+Test Cases
+Input	Expected Output
+Python	P and n
+Computer	C and r
+Hello	H and o
+
+--ANSWERS:
+word=input("Enter here :")
+print(word[0])
+print(word[-1])
+print(word.lower())
+
+
+
+Q23. Think Carefully About Indexing
+Given:
+
+word = "PROGRAM"
+Without running the code, determine:
+
+word[0]
+word[2]
+word[-1]
+word[-4]
+--ANSWERS:
+P
+O
+M
+G
+

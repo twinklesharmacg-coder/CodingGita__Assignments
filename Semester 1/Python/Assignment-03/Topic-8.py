5cafcfa86b6431651d1f64aecd6c2d3c75495b70
Twinkle <twinkle.sharma.cg@gmail.com>
2026-09-22 21:49:25 +0530

    Add Python escape sequences exercises

---
+++ Semester 1/Python/Assignment-03/Topic-8.py
@@ -0,0 +1,100 @@
+## Q42. New Line
+
+Write a statement that produces exactly:
+
+```text
+Hello
+World
+```
+
+Use `\n`.
+
+---Answer:
+print(f"Hello \n World")
+
+## Q43. Tab
+
+Write a program that produces:
+
+```text
+Name:   Rahul
+Age:    20
+City:   Ahmedabad
+```
+
+Use `\t`.
+
+---Answer:
+print("Name:\tRahul")
+print("Age:\t20")
+print("City:\tAhmedabad")
+
+## Q44. Backslash
+
+Write a program that displays exactly:
+
+```text
+C:\Python\Programs
+```
+
+Use `\\`.
+
+---Answer:
+
+print("C:\\Python\\Programs")
+
+## Q45. Single Quote
+
+Write a statement that displays:
+
+```text
+It's Python
+```
+
+Use an appropriate escape sequence.
+
+---Answer:
+print("It's Python")
+
+## Q46. Double Quote
+
+Write a statement that displays:
+
+```text
+He said "Hello"
+```
+
+Use an appropriate escape sequence.
+
+---Answer:
+print('He said "Hello"')
+
+
+## Q47. Predict the Output
+
+```python
+print("Python\nProgramming")
+```
+
+---Output:
+Python
+Programming
+
+## Q48. Combined Escape Sequences
+
+Write a program that displays:
+
+```text
+Student Details
+
+Name:   Rahul
+Age:    20
+Course: B.Tech
+```
+Use `\n` and `\t`.
+
+---Answer:
+print("Student details")
+print(f"Name:\t Rahul \n Age: \t 20 \n Course:\t B.Tech")
+
+
